6eb45f0b638ac4d5200de2f2ff7594148a9214fe
euni1004 <kongeuni1004@gmail.com>
2022-10-21 09:16:51 +0900

    first

---
+++ src/main/java/com/week06/team01_week06_project/Team01Week06ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.week06.team01_week06_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team01Week06ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team01Week06ProjectApplication.class, args);
+    }
+
+}
