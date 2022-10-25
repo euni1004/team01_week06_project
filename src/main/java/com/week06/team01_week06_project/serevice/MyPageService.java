558d809eae6e8a89db6c9d168473d4c4c0ed859f
euni1004 <kongeuni1004@gmail.com>
2022-10-25 15:27:10 +0900

    마이페이지 추가

--- src/main/java/com/week06/team01_week06_project/serevice/MyPageService.java
+++ src/main/java/com/week06/team01_week06_project/serevice/MyPageService.java
@@ -8,10 +8,13 @@ import com.week06.team01_week06_project.dto.response.GamePostResDto;
 import com.week06.team01_week06_project.exception.CustomException;
 import com.week06.team01_week06_project.exception.ErrorCode;
 import com.week06.team01_week06_project.respository.MemberRepository;
+import com.week06.team01_week06_project.s3.AmazonS3ResourceStorage;
 import com.week06.team01_week06_project.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,27 +22,47 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MyPageService {
     private final MemberRepository memberRepository;
+    private final AmazonS3ResourceStorage amazonS3ResourceStorage;
+    private final GamePostService gamePostService;
 
-    public GlobalResDto<List<GamePostResDto>> getMyPost(UserDetailsImpl userDetails){
+    public GlobalResDto<List<GamePostResDto>> getMyPost(UserDetailsImpl userDetails) throws ParseException {
         Member member = memberRepository.findByUserid(userDetails.getAccount().getUserid()).orElseThrow(
                 () -> new CustomException(ErrorCode.NOT_FOUND_MEMBER)
         );
         List<GamePost> myGamePosts = member.getGamePost();
         List<GamePostResDto> myGamePostList = new ArrayList<>();
         for(GamePost gamePost : myGamePosts){
-            myGamePostList.add(GamePostResDto.myPost(gamePost));
+            String countTime = gamePostService.countDate(gamePost.getCreatedAt());
+            String postTime = gamePost.getCreatedAt().format(DateTimeFormatter.ofPattern("M월 d일 h시 m분"));
+            String imgurl = amazonS3ResourceStorage.getimg(gamePost.getPath());
+            if (gamePost.getRecruitStatus()){
+                myGamePostList.add(GamePostResDto.toGamePostResDto(postTime, countTime,gamePost,imgurl));
+            } else {
+                List<String> inGameNickname = gamePostService.isPresentNickname(gamePost);
+                inGameNickname.add(0, gamePost.getMyIngameNickname());
+                myGamePostList.add(GamePostResDto.toDoneGamePostResDto(postTime, countTime,gamePost,inGameNickname,imgurl));
+            }
         }
         return GlobalResDto.success(myGamePostList);
     }
 
-    public GlobalResDto<List<GamePostResDto>> getMyRecruit(UserDetailsImpl userDetails) {
+    public GlobalResDto<List<GamePostResDto>> getMyRecruit(UserDetailsImpl userDetails) throws ParseException {
         Member member = memberRepository.findByUserid(userDetails.getAccount().getUserid()).orElseThrow(
                 () -> new CustomException(ErrorCode.NOT_FOUND_MEMBER)
         );
         List<RecruitStatus> myRecruit = member.getRecruitStatus();
         List<GamePostResDto> myGameRecruitList = new ArrayList<>();
-        for(RecruitStatus gamePost : myRecruit){
-            myGameRecruitList.add(GamePostResDto.myPost(gamePost.getGamePost()));
+        for(RecruitStatus recruit : myRecruit){
+            String countTime = gamePostService.countDate(recruit.getGamePost().getCreatedAt());
+            String postTime = recruit.getGamePost().getCreatedAt().format(DateTimeFormatter.ofPattern("M월 d일 h시 m분"));
+            String imgurl = amazonS3ResourceStorage.getimg(recruit.getGamePost().getPath());
+            if (recruit.getGamePost().getRecruitStatus()){
+                myGameRecruitList.add(GamePostResDto.toGamePostResDto(postTime, countTime,recruit.getGamePost(),imgurl));
+            } else {
+                List<String> inGameNickname = gamePostService.isPresentNickname(recruit.getGamePost());
+                inGameNickname.add(0, recruit.getGamePost().getMyIngameNickname());
+                myGameRecruitList.add(GamePostResDto.toDoneGamePostResDto(postTime, countTime,recruit.getGamePost(),inGameNickname,imgurl));
+            }
         }
         return GlobalResDto.success(myGameRecruitList);
     }
